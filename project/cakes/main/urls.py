e992127d7b350305b1c7dc2c78ab8bfcc666d17b
yuya <nerentabelen@mail.ru>
2024-04-30 02:15:55 +0300

    add urls and view functions

--- project/cakes/main/urls.py
+++ project/cakes/main/urls.py
@@ -4,4 +4,13 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('cake_info/', cake_info, name='cake_info'),
+    path('account/', account, name='account'),
+    path('registration_form/', registration_form, name='registration_form'),
+    path('login_form/', login_form, name='login_form'),
+    path('recovery_form/', recovery_form, name='recovery_form'),
+    path('catalog/', catalog, name='catalog'),
+    path('cart/', cart, name='cart'),
+    path('order_form/', order_form, name='order_form'),
+    path('order_details/', order_details, name='order_details'),
 ]
--- project/cakes/main/views.py
+++ project/cakes/main/views.py
@@ -5,5 +5,25 @@ from django.db.models import Sum, Count, F
 
 
 def home(request):
-
     return render(request, 'main/home.html')
+
+def cake_info(request):
+    return render(request, 'main/cake_info.html')
+
+def account(request):
+    return render(request, 'main/account.html')
+
+def registration_form(request):
+    return render(request, 'main/registration_form.html')
+def login_form(request):
+    return render(request, 'main/login_form.html')
+def recovery_form(request):
+    return render(request, 'main/recovery_form.html')
+def catalog(request):
+    return render(request, 'main/catalog.html')
+def cart(request):
+    return render(request, 'main/cart.html')
+def order_form(request):
+    return render(request, 'main/order_form.html')
+def order_details(request):
+    return render(request, 'main/order_details.html')
