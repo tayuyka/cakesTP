a9fe8003c3898c1247527f93f710b30877079b41
yuya <nerentabelen@mail.ru>
2024-05-25 17:04:39 +0300

    add all models to admin

--- project/cakes/main/admin.py
+++ project/cakes/main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Cake, CakeAddition, CakeCoverage, CakeShape, CakeSize, CakeStructure, CakeTopping, Layer, LayerBase, LayerFilling, Order, OrderContent, User
 
-# Register your models here.
+admin.site.register(Cake)
+admin.site.register(CakeAddition)
+admin.site.register(CakeCoverage)
+admin.site.register(CakeShape)
+admin.site.register(CakeSize)
+admin.site.register(CakeStructure)
+admin.site.register(CakeTopping)
+admin.site.register(Layer)
+admin.site.register(LayerBase)
+admin.site.register(LayerFilling)
+admin.site.register(Order)
+admin.site.register(OrderContent)
+admin.site.register(User)
